002448c4da6b2444b8c9436e7b9ba7dfbad76e8c
sherman07 <shermanzheng8@gmail.com>
2026-04-26 19:41:35 +1200

    Initialize the client with api key

--- app.py
+++ app.py
@@ -1,11 +1,37 @@
+import os
 import streamlit as st
+from dotenv import load_dotenv
+from openai import OpenAI
+
+# Read .env
+load_dotenv()
+
+# Initialize client
+client = OpenAI(
+    api_key=os.getenv("OPENAI_API_KEY")
+)
 
 st.title("AI Study Assistant")
 
 question = st.text_input("Ask something:")
 
 if question:
-    st.write("You asked:", question)
+    with st.spinner("Thinking..."):
+        response = client.chat.completions.create(
+            model="gpt-4o-mini",
+            messages=[
+                {
+                    "role":"system",
+                    "content":
+                    "You are an academic assistant helping university students understand readings."
+                },
+                {
+                    "role":"user",
+                    "content":question
+                }
+            ]
+        )
 
-### To run this Streamlit app, use the following command:
-### streamlit run app.py
+    answer = response.choices[0].message.content
+
+    st.write(answer)
